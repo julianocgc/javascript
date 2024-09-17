e1d71933cc1f752e028bcf15f66c3a35044f165c
Juliano Correa <juliano.correa@sou.unifeob.edu.br>
2024-09-17 19:31:59 -0300

    Initial commit

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+function parimpar(n){
+    if (n % 2 == 0){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+//chamando a função parimpar e passando o parâmetro 220
+console.log(parimpar(220))
---
+++ aula17/objeto01.js
@@ -0,0 +1,2 @@
+let amigo = {nome: 'José', Sexo: 'M', peso: 85.4}
+console.log(amigo)
